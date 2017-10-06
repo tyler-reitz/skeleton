2dd771da18a8f3195d23bdbb30549c61f088fb48
Tyler Reitz <tdreitz@gmail.com>
2017-10-06 01:29:08 -0700

    Node, Yarn, and package.json

--- client/app/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import Main from './components'
-
-const App = () => <Main />
-
-export default App
--- server.js
+++
@@ -1,4 +0,0 @@
-const app = require('./server/index')
-const PORT = process.env.PORT || 8081
-
-app.listen(PORT, () => console.log(`App running on port: ${PORT}`))
