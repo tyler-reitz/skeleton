5e00e70fdf219334a9cd2691d80e98aa84d39829
Tyler Reitz <tdreitz@gmail.com>
2017-10-06 11:02:52 -0700

    Redux, Immutable, and Fetch

--- src/server/index.js
+++ src/server/index.js
@@ -7,6 +7,8 @@ import { APP_NAME, STATIC_PATH, WEB_PORT } from '../shared/config'
 import { isProd } from '../shared/util'
 import renderApp from './render-app'
 
+import { helloEndpointRoute } from '../shared/routes'
+
 const app = express()
 
 app.use(compression())
@@ -17,8 +19,15 @@ app.get('/', (req, res) => {
   res.send(renderApp(APP_NAME))
 })
 
+app.get(helloEndpointRoute(), (req, res) => {
+  res.json({ serverMessage: `Hello from server! (received ${req.params.num})` })
+})
+
 app.listen(WEB_PORT, () =>
   console.log(
-    `Server running on port ${WEB_PORT} ${isProd ? '(production)' : '(dev)'}`,
+    `Server running on port ${WEB_PORT} ${isProd
+      ? '(production)'
+      : '(dev).\nKeep "npm run dev:wds" running in another terminal'
+    }.`,
   ),
 )
